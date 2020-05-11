366c41ad968176500d26bc0df39453ff5602ee15
”伊東淳” <j.style.0519.i@gmail.com>
2020-05-11 11:39:16 +0900

    ユーザー登録画面作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
