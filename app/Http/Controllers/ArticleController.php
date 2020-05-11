6a6bf931d5e95c7f7045cd21d3d71efef51a2cc5
”伊東淳” <j.style.0519.i@gmail.com>
2020-05-11 14:21:54 +0900

    更新と削除の作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -29,4 +29,21 @@ class ArticleController extends Controller
         // $request->photo->storeAs('public/article_images', $article->id . '.jpg');
         return redirect()->route('articles.index');
     }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+    
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
 }
