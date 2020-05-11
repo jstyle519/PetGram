4701b1e86d6c04aaaab26a4b41196f2d61d842a6
”伊東淳” <j.style.0519.i@gmail.com>
2020-05-11 11:55:17 +0900

    ユーザー登録機能完成

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,38 +4,46 @@
 
   <ul class="navbar-nav ml-auto">
 
+    @guest
     <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
+    @endguest
 
+    @guest
     <li class="nav-item">
       <a class="nav-link" href="">ログイン</a>
     </li>
+    @endguest
 
+    @auth
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
+    @endauth
 
+    @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
-        <i class="fas fas fa-dog"></i>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button"
-                onclick="location.href=''">
-          マイページ
-        </button>
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
+      aria-haspopup="true" aria-expanded="false">
+      <i class="fas fas fa-dog"></i>
+    </a>
+    <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+      <button class="dropdown-item" type="button"
+      onclick="location.href=''">
+      マイページ
+    </button>
+    <div class="dropdown-divider"></div>
+    <button form="logout-button" class="dropdown-item" type="submit">
+      ログアウト
+    </button>
+    </div>
     </li>
     <form id="logout-button" method="POST" action="">
     </form>
     <!-- Dropdown -->
+    @endauth
 
   </ul>
 
