d1ca4ee099a901429b9999b176c3ce9eb65482e0
Jun Ito <j.style.0519.i@gmail.com>
2020-05-25 17:22:46 +0900

    footer追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,6 +24,10 @@
 
   <script src="{{ mix('js/app.js') }}"></script>
 
+
+  <footer>
+    <div class="footer mt-5 text-center">Copyright&copy;JUN ITO. All Rights Reserved.</div>
+  </footer>
   <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
@@ -33,5 +37,7 @@
   <!-- MDB core JavaScript -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
 </body>
+
+
 
 </html>
