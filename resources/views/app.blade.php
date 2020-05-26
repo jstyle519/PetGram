70949439be6dd9f19d21b69d587daf4b006e5ec4
Jun Ito <j.style.0519.i@gmail.com>
2020-05-26 16:46:59 +0900

    プロフィール画像のマイグレーション

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Pet Gram</title>
-  <meta name="description" content="自慢のペットの写真を載せよう Pet Gram">
+  <meta name="description" content="自慢のペットの写真を載せよう Petに特化したSNSサービス ~Pet Gram~">
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
