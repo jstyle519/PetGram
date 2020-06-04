6cff4cbe872b78ce061d970afeb34116401bc3ac
Jun Ito <j.style.0519.i@gmail.com>
2020-06-04 23:15:45 +0900

    form.blade.php詳細編集

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,8 +1,11 @@
+{{-- 記事投稿画面 --}}
 @csrf
+  {{-- タイトル --}}
   <div class="md-form">
     <label>☆タイトル</label>
     <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
   </div>
+  {{-- タグ --}}
   <div class="form-group mt-2">
     <article-tags-input
     :initial-tags='@json($tagNames ?? [])'
@@ -10,11 +13,12 @@
     >
     </article-tags-input>
   </div>
+  {{-- 本文 --}}
   <div class="form-group">
     <label></label>
     <textarea name="body" required class="form-control" rows="16" placeholder="☆本文">{{ $article->body ?? old('body') }}</textarea>
 
-{{-- 画像   --}}
+  {{-- 画像   --}}
     <div class="form-group row">
       <div class="col-md-10 mt-2">
         <label>☆自慢のペットの写真を添付してください。</label><br>
@@ -22,6 +26,7 @@
         <input type="file" name="image" accept="image/jpeg,image/gif,image/png" />
       </div>
     </div>
+    {{-- アラートさせる --}}
     <script type="text/javascript">
       $('#article_image').bind('change', function() {
         var size_in_megabytes = this.files[0].size/1024/1024;
