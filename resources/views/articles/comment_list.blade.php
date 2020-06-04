e967dac652fb27497b828fbb5cc83528d86b79f6
Jun Ito <j.style.0519.i@gmail.com>
2020-06-04 23:09:35 +0900

    comment_list.blade.phpの詳細追加

--- resources/views/articles/comment_list.blade.php
+++ resources/views/articles/comment_list.blade.php
@@ -1,8 +1,7 @@
+{{-- コメント機能を表示させる --}}
 @foreach ($article->comments as $comment) 
   <div class="mb-2">
-    {{-- @if ($comment->user->id == Auth::user()->id) --}}
       <a class="delete-comment"  data-remote="true" rel="nofollow" data-method="delete" href="/comments/{{ $comment->id }}"></a>
-    {{-- @endif --}}
     <span>
       <strong>
         <a class="no-text-decoration black-color" href="{{ route('users.show', ['name' => $article->user->name]) }}">
