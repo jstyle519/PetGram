6de60b12597161b37105404b70753743e2cac056
Jun Ito <j.style.0519.i@gmail.com>
2020-06-05 15:12:07 +0900

    web.php編集

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Auth route
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
@@ -20,18 +20,22 @@ Route::resource('/articles', 'ArticleController')->only(['show']);
 Route::post('/articles/{comment_id}/comments','CommentsController@store');
 //コメント取消処理
 Route::get('/comments/{comment_id}', 'CommentsController@destroy');
-
+// Like
 Route::prefix('articles')->name('articles.')->group(function () {
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
-
+// tagu
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 Route::prefix('users')->name('users.')->group(function () {
     Route::get('/{name}', 'UserController@show')->name('show');
+    // like
     Route::get('/{name}/likes', 'UserController@likes')->name('likes');
+    // フォロー
     Route::get('/{name}/followings', 'UserController@followings')->name('followings');
+    // フォロワー
     Route::get('/{name}/followers', 'UserController@followers')->name('followers');
+    // フォロー・フォロワーdelete
     Route::middleware('auth')->group(function () {
         Route::put('/{name}/follow', 'UserController@follow')->name('follow');
         Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
