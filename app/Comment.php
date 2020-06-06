85f7271d11fbf7129b404eeb4247f4c3c32b379f
Jun Ito <j.style.0519.i@gmail.com>
2020-06-06 22:05:01 +0900

    Comment.php変更

--- app/Comment.php
+++ app/Comment.php
@@ -6,15 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+
     protected $fillable = [
         'text', 'article_id',
     ];
-
+    // articleリレーション
     public function article()
     {
         return $this->belongsTo('App\Article');
     }
 
+    // userリレーション
     public function user()
     {
         return $this->belongsTo('App\User');
